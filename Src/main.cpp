cd4ba77811f7c031d555c14af1a577fce8e23536
Andreas Belke <andreas.belke@igniti.de>
2021-03-06 23:16:01 +0100

    change: layout for android

--- Src/main.cpp
+++ Src/main.cpp
@@ -8,10 +8,14 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     QCoreApplication::setOrganizationName("dilishes");
     QCoreApplication::setOrganizationDomain("dilishes.de");
-    QCoreApplication::setApplicationName("YearsAgo");
+    QCoreApplication::setApplicationName("Years Ago");
     QGuiApplication::setApplicationDisplayName("Years ago");
 
     View w;
+#ifdef Q_OS_ANDROID
+    w.showFullScreen();
+#else
     w.show();
+#endif
     return a.exec();
 }
