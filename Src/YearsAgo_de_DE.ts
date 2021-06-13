98379d60d9f7c7c2ce761eef7374caa16448697b
Andreas Belke <andreas.belke@igniti.de>
2021-06-13 21:31:32 +0200

    add: ts file

---
+++ Src/YearsAgo_de_DE.ts
@@ -0,0 +1,89 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>View</name>
+    <message>
+        <location filename="View.ui" line="14"/>
+        <source>View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View.ui" line="29"/>
+        <source>CurrentDate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View.ui" line="55"/>
+        <source>Years ago:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View.ui" line="62"/>
+        <source>SearchDate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View.ui" line="69"/>
+        <source>has</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View.ui" line="76"/>
+        <source>MatchCount</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View.ui" line="83"/>
+        <source>matches</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View.ui" line="90"/>
+        <source>Show Next</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View.ui" line="97"/>
+        <source>Share</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View.ui" line="141"/>
+        <source>Image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View.ui" line="154"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View.ui" line="160"/>
+        <source>Image Folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View.ui" line="174"/>
+        <source>Browse</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View.cpp" line="39"/>
+        <location filename="View.cpp" line="47"/>
+        <source>Year ago</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View.cpp" line="40"/>
+        <location filename="View.cpp" line="48"/>
+        <source>Years ago</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="View.cpp" line="108"/>
+        <source>Open Directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
